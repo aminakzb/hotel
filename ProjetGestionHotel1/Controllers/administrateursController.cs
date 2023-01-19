f1e94154caea893ecd0261f05e5cabc060b5081e
AMINA KZIBER <aminaaakz123@gmail.com>
2023-01-19 21:25:41 +0100

    third commit

---
+++ ProjetGestionHotel1/Controllers/administrateursController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetGestionHotel1.Models;
+
+namespace ProjetGestionHotel.Controllers
+{
+    public class administrateursController : Controller
+    {
+        private gestion_hotelEntities db = new gestion_hotelEntities();
+
+        // GET: administrateurs
+        public ActionResult Index()
+        {
+            Session["nbr_client"] = db.client.Count();
+            Session["comments_negative"] = db.commentaire.Where(c => c.score == 0).Count();
+            Session["comments_positive"] = db.commentaire.Where(c => c.score == 1).Count();
+            return View(db.administrateur.ToList());
+        }
+
+        // GET: administrateurs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                administrateur admin = (administrateur)@Session["user"];
+                id = admin.id_admin;
+
+            }
+            administrateur administrateur = db.administrateur.Find(id);
+            if (administrateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administrateur);
+        }
+
+        // GET: administrateurs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: administrateurs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Exclude = "photo_profil")]administrateur administrateur)
+        {
+            if (ModelState.IsValid)
+            {
+                var checkEmailAdmin = db.administrateur.FirstOrDefault(a => a.email_admin.Equals(administrateur.email_admin));
+                var checkLoginAdmin = db.administrateur.FirstOrDefault(a => a.login_admin.Equals(administrateur.login_admin));
+                byte[] imageData = null;
+                if (checkEmailAdmin == null && checkLoginAdmin == null)
+                {
+                    if (Request.Files.Count > 0)
+                    {
+                        HttpPostedFileBase poImgFile = Request.Files["photo_profil"];
+                        using (var binary = new BinaryReader(poImgFile.InputStream))
+                        {
+                            if (poImgFile.ContentLength != 0)
+                                imageData = binary.ReadBytes(poImgFile.ContentLength);
+                            else
+                            {
+                                string path = "C:\\Users\\azzai\\source\\repos\\ProjetGestionHotel1\\ProjetGestionHotel1\\Content\\images\\thisavatar.png";
+                                imageData = System.IO.File.ReadAllBytes(path);
+                            }
+
+                        }
+                    }
+                    administrateur.is_superadmin = false;
+                    administrateur.photo_profil = imageData;
+                    db.administrateur.Add(administrateur);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "administrateurs");
+                }
+                else if (checkLoginAdmin != null)
+                {
+                    ViewBag.error = "Login already exist !";
+                    return View();
+                }
+                else if (checkEmailAdmin != null)
+                {
+                    ViewBag.error = "Email already exist !";
+                    return View();
+                }
+            }
+            return View(administrateur);
+        }
+
+        // GET: administrateurs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                administrateur admin = (administrateur)@Session["user"];
+                id = admin.id_admin;
+            }
+            administrateur administrateur = db.administrateur.Find(id);
+            if (administrateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(administrateur);
+        }
+
+        // POST: administrateurs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(administrateur administrateur)
+        {
+            if (ModelState.IsValid)
+            {    
+                db.Entry(administrateur).State = EntityState.Modified;
+                db.SaveChanges();
+                TempData["message"] = "Modifié avec succès";
+                return RedirectToAction("Index");
+            }
+            return View(administrateur);
+        }
+
+        // GET: administrateurs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            administrateur administrateur = db.administrateur.Find(id);
+            if (administrateur == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                db.administrateur.Remove(administrateur);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjetGestionHotel1/Controllers/imagesController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjetGestionHotel1.Models;
+
+namespace ProjetGestionHotel1.Controllers
+{
+    public class imagesController : Controller
+    {
+        private gestion_hotelEntities db = new gestion_hotelEntities();
+
+        // GET: images
+        public ActionResult Index()
+        {
+            var images = db.images.Include(i => i.chambre);
+            return View(images.ToList());
+        }
+
+        // GET: images/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            images images = db.images.Find(id);
+            if (images == null)
+            {
+                return HttpNotFound();
+            }
+            return View(images);
+        }
+
+        // GET: images/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_chambre = new SelectList(db.chambre, "id_chambre", "disponibilite");
+            return View();
+        }
+
+        // POST: images/Create
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Exclude = "image_file")] images images)
+        {
+            if (ModelState.IsValid)
+            {
+                byte[] imageData = null;
+                if (Request.Files.Count > 0)
+                {
+                    HttpPostedFileBase poImgFile = Request.Files["image_file"];
+                    using (var binary = new BinaryReader(poImgFile.InputStream))
+                    {
+                        if (poImgFile.ContentLength != 0)
+                            imageData = binary.ReadBytes(poImgFile.ContentLength);
+                        else
+                        {
+                            string path = "C:\\Users\\LENOVO THINKPAD\\OneDrive\\Bureau\\app-gestion-hotel-master\\ProjetGestionHotel1\\Content\\images\\thisavatar.png";
+                            imageData = System.IO.File.ReadAllBytes(path);
+                        }
+                    }
+                }
+                images.id_chambre =(int) @Session["chambre"];
+                images.image_file = imageData;
+                db.images.Add(images);
+                db.SaveChanges();
+                return RedirectToAction("Index","images");
+            }
+
+            ViewBag.id_chambre = new SelectList(db.chambre, "id_chambre", "disponibilite", images.id_chambre);
+            return View(images);
+        }
+
+        // GET: images/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            images images = db.images.Find(id);
+            if (images == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_chambre = new SelectList(db.chambre, "id_chambre", "disponibilite", images.id_chambre);
+            return View(images);
+        }
+
+        // POST: images/Edit/5
+        // Afin de déjouer les attaques par survalidation, activez les propriétés spécifiques auxquelles vous voulez établir une liaison. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_img,id_chambre,image_file")] images images)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(images).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_chambre = new SelectList(db.chambre, "id_chambre", "disponibilite", images.id_chambre);
+            return View(images);
+        }
+
+        // GET: images/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            images images = db.images.Find(id);
+            if (images == null)
+            {
+                return HttpNotFound();
+            }
+            return View(images);
+        }
+
+        // POST: images/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            images images = db.images.Find(id);
+            db.images.Remove(images);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
